467eb72a4d904ae4d6d9b2dda5f306cd0db329fe
Jonathan Hudak <hudak.jonathan@gmail.com>
2018-11-18 13:23:39 -0800

    Initial setup

---
+++ components/Button.story.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Button from './Button';
+
+storiesOf('Button', module)
+  .add('default', () => (
+    <Button onClick={action('clicked')}>Default Button</Button>
+  ))
+  .add('primary', () => (
+    <Button onClick={action('clicked')} primary>Primary Button 😎</Button>
+  ));
