0f0d0755ba7da6d6ffb4b8896b9c870c93da11b4
Jonathan Hudak <hudak.jonathan@gmail.com>
2019-10-13 11:59:39 -0700

    gh website deploy

--- docs.js
+++ docs.js
@@ -1,15 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import mdx from './src/**/*.mdx';
 
 const root = document.getElementById('root');
+
+const rootPath = process.env.ROOT || '/';
 
 function App() {
   return (
@@ -17,25 +14,29 @@ function App() {
       <div>
         <nav>
           <ul>
-            {Object.keys(mdx).map(path => <li key={path}><Link to={`/${path}`}>{path}</Link></li>)}
+            {Object.keys(mdx).map(path => (
+              <li key={path}>
+                <Link to={`${path}`}>{path}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
         <Switch>
           {Object.entries(mdx).map(([path, module]) => {
             const RouteComponent = module[path].default;
             return (
-              <Route path={`/${path}`} key={path}>
-            <RouteComponent />
-          </Route>
-            )
+              <Route path={`${rootPath}${path}`} key={path}>
+                <RouteComponent />
+              </Route>
+            );
           })}
-          <Route path="/">
-            <div>froo</div>
+          <Route path={rootPath}>
+            <div>home</div>
           </Route>
         </Switch>
       </div>
     </Router>
-  )
+  );
 }
 
 render(<App />, root);
